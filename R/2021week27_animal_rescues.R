d7d4127116877a96355cd5d75a46b26ea999c2ff
emmaSkarstein <36135547+emmaSkarstein@users.noreply.github.com>
2021-10-25 15:24:16 +0200

    added animal rescue

---
+++ R/2021week27_animal_rescues.R
@@ -0,0 +1,170 @@
+# Animal rescues, TidyTuesday 2021, week 27
+# By Emma Skarstein, October 2021
+
+library(tidyverse)
+library(showtext)
+library(myGraphicsToolkit)
+library(sf)
+library(maptools)
+library(ggtext)
+library(wesanderson)
+library(patchwork)
+
+
+animal_rescues <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-06-29/animal_rescues.csv')
+
+animal_rescues$animal_group_parent <- fct_collapse(animal_rescues$animal_group_parent, Cat = c("Cat", "cat"))
+animal_rescues$special_service_type_category <- fct_recode(animal_rescues$special_service_type_category,
+                                                           Height = "Animal rescue from height",
+                                                           Below_ground = "Animal rescue from below ground",
+                                                           Water = "Animal rescue from water",
+                                                           Other = "Other animal assistance")
+
+# Select animals of interest. What are the most common animals?
+animal_counts <- animal_rescues %>% group_by(animal_group_parent) %>% count()
+
+
+# Most common animals are cats and birds. Choose to look at cats,
+# and remove 2021 observations since year is not over.
+cats <- animal_rescues %>% filter(animal_group_parent == "Cat", cal_year < 2021)
+
+yearly_service_counts <- cats %>% group_by(special_service_type_category) %>%
+  count(cal_year) %>% pivot_wider(names_from = "special_service_type_category", values_from = "n")
+
+# Map
+london <- readShapePoly("R/spatialggplot/london_sport.shp")  # read in the shapefile
+proj4string(london) <- CRS("+init=epsg:27700")
+
+# Font
+f1 <- "Oswald"
+f2 <- "Open Sans"
+font_add_google(name = f1, family = f1)
+font_add_google(name = f2, family = f2)
+showtext_auto()
+
+# Colors
+cat_pal5 <- '["e3b505","95190c","610345","107e7d"]'
+col_points <- myGraphicsToolkit::CreateRPalette(cat_pal5, n = 4)
+col_points <- wes_palette("Royal1")
+col_bg <- "gray10"
+col_panel <- "gray20"
+col_text <- "gray70"
+col_lines <- "gray30"
+
+
+# Some scalars for the bar
+x0 <- 498000
+y0 <- 157000
+scalen <- 130
+width <- 2
+
+catplot <- ggplot(cats) +
+  # Background map
+  geom_polygon(data = london, aes(long, lat, group = group), fill = "grey30", color = "grey40") +
+  # Points for animal rescues
+  geom_point(aes(x = easting_rounded, y = northing_rounded, fill = special_service_type_category),
+             pch = 21, color = "black") +
+  scale_fill_manual(values = col_points) +
+  #geom_point(data = yearly_service_counts, aes(x = 500000+5000*(special_service_number-1), y = 160000, size = n, color = special_service_type_category)) +
+  # The bar that shows total numbers
+  geom_segment(data = yearly_service_counts,
+               aes(x = x0, xend = x0, y = y0, yend = y0 + scalen*Below_ground),
+               size = width, color = col_points[1]) +
+  geom_segment(data = yearly_service_counts,
+               aes(x = x0, xend = x0, y = y0 + scalen*Below_ground, yend = y0 + scalen*(Below_ground + Height)),
+               size = width, color = col_points[2]) +
+  geom_segment(data = yearly_service_counts,
+               aes(x = x0, xend = x0, y = y0 + scalen*(Below_ground + Height), yend = y0 + scalen*(Below_ground + Height + Water)),
+               size = width, color = col_points[3]) +
+  geom_segment(data = yearly_service_counts,
+               aes(x = x0, xend = x0, y = y0 + scalen*(Below_ground + Height + Water), yend = y0 + scalen*(Below_ground + Height + Water + Other) ),
+               size = width, color = col_points[4]) +
+  geom_text(data = yearly_service_counts,
+            aes(x = x0 + 2600, y = y0 + scalen*(Below_ground + Height + Water + Other) + 3800,
+                label = Below_ground + Height + Water + Other), color = col_text,
+            family = f2, face = "bold") +
+  labs(title = "2020 MARKS ALL-TIME HIGH FOR CAT RESCUES IN LONDON",
+       subtitle = "The maps show yearly cat rescues
+       <span style='color:#899DA4;'>below ground</span>,
+       <span style='color:#C93312;'>from a hight</span>,
+       <span style='color:#FAEFD1;'>from water</span> or
+       <span style='color:#DC863B;'>other</span>.",
+       caption = "Source: London.gov  |  Visualization: Emma Skarstein") +
+  facet_wrap(vars(cal_year)) +
+  coord_equal() +
+  my_basic_theme(base_family = f2) +
+  theme(text = element_text(family = f2, size = 18, color = col_text),
+        plot.title = element_markdown(family = f1,
+                                  size = 26,
+                                  face = "bold",
+                                  margin = margin(b = 5)),
+        plot.subtitle = element_markdown(size = 15,
+                                     margin = margin(b = 10)),
+        plot.caption = element_text(size = 12,
+                                    margin = margin(t = 10)),
+        strip.text = element_text(size = 15,
+                                  face = "bold",
+                                  color = col_bg),
+        axis.text.x = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks = element_blank(),
+        plot.background = element_rect(fill = col_bg, color = col_bg),
+        panel.background = element_rect(fill = col_panel, color = col_panel),
+        legend.position = "none",
+        panel.spacing = unit(.05, "lines"),
+        panel.border = element_rect(color = col_lines, fill = NA, size = 1),
+        strip.background = element_rect(color = col_lines, fill = "grey40", size = 1),
+        plot.margin = margin(20, 20, 5, 20))
+
+ggsave("output/2021week27_animal_rescue.pdf", width = 11, height = 8)
+ggsave("output/2021week27_animal_rescue.png", width = 11, height = 8)
+
+
+# Compare with dog rescues
+cat_dog <- animal_rescues %>% filter(animal_group_parent %in% c("Cat", "Dog"), cal_year < 2021)
+cat_dog$special_service_type_category <- fct_recode(cat_dog$special_service_type_category,
+                                                 Height = "Animal rescue from height",
+                                                 "Below_ground" = "Animal rescue from below ground",
+                                                 Water = "Animal rescue from water",
+                                                 Other = "Other animal assistance")
+
+cat_dog_plot <- ggplot(cat_dog, aes(y = animal_group_parent, fill = special_service_type_category)) +
+  geom_bar() +
+  scale_fill_manual(values = col_points) +
+  facet_wrap(vars(cal_year)) +
+  my_basic_theme(base_family = f2) +
+  theme(text = element_text(family = f2, size = 18, color = col_text),
+        plot.title = element_markdown(family = f1,
+                                      size = 26,
+                                      face = "bold",
+                                      margin = margin(b = 5)),
+        plot.subtitle = element_markdown(size = 15,
+                                         margin = margin(b = 10)),
+        plot.caption = element_text(size = 12,
+                                    margin = margin(t = 10)),
+        strip.text = element_text(size = 15,
+                                  face = "bold",
+                                  color = col_bg),
+        axis.text.x = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks = element_blank(),
+        plot.background = element_rect(fill = col_bg, color = col_bg),
+        panel.background = element_rect(fill = col_panel, color = col_panel),
+        legend.position = "none",
+        panel.spacing = unit(.05, "lines"),
+        panel.border = element_rect(color = col_lines, fill = NA, size = 1),
+        strip.background = element_rect(color = col_lines, fill = "grey40", size = 1),
+        plot.margin = margin(20, 20, 5, 20))
+
+
+catplot/cat_dog_plot
+
+
+ggsave("output/2021week27_animal_rescue.pdf")
+ggsave("output/2021week27_animal_rescue.png")
+
+# Idea: make a shiny app where you can see a map and some summary statistics for a user specified year and animal (let totals be an option as well)
+library(shiny)
+
+
+
---
+++ R/2021week27_animal_rescues/app.R
@@ -0,0 +1,147 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(shinyWidgets)
+library(tidyverse)
+library(showtext)
+library(myGraphicsToolkit)
+library(sf)
+library(maptools)
+library(ggtext)
+library(wesanderson)
+library(patchwork)
+library(gghighlight)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+    setBackgroundColor(color = "white"),
+    tags$style(HTML(".js-irs-0 .irs-single, .js-irs-0 .irs-bar-edge, .js-irs-0 .irs-bar {background: #899DA4}")),
+
+    # Application title
+    titlePanel(""),
+
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("year",
+                        "Year:",
+                        min = 2009,
+                        max = 2020,
+                        value = 2020,
+                        step = 1,
+                        animate = TRUE,
+                        sep = ""),
+            selectInput("animal", "Select animal:",
+                        choices = list("Cat" = "Cat", "Dog" = "Dog", "Bird" = "Bird"),
+                        selected = "Cat")
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput("distPlot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    # Selected animal and year
+    animal_rescues <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-06-29/animal_rescues.csv')
+    animal_rescues$animal_group_parent <- fct_collapse(animal_rescues$animal_group_parent, Cat = c("Cat", "cat"))
+    animal_rescues$special_service_type_category <- fct_recode(animal_rescues$special_service_type_category,
+                                                               Height = "Animal rescue from height",
+                                                               Below_ground = "Animal rescue from below ground",
+                                                               Water = "Animal rescue from water",
+                                                               Other = "Other animal assistance")
+
+    selected_animal_year <- reactive({
+        animal_rescues %>%
+            filter(animal_group_parent == input$animal,
+                   cal_year == input$year)
+        })
+    across_time <- reactive({
+        animal_rescues %>%
+            filter(animal_group_parent == input$animal,
+                   cal_year < 2021)
+    })
+
+    # Map
+    #london <- readShapePoly("../spatialggplot/london_sport.shp")  # read in the shapefile
+    #proj4string(london) <- CRS("+init=epsg:27700")
+
+    # Font
+    f1 <- "Oswald"
+    f2 <- "Open Sans"
+    font_add_google(name = f1, family = f1)
+    font_add_google(name = f2, family = f2)
+    showtext_auto()
+
+    # Colors
+    col_points <- wes_palette("Royal1")
+    col_bg <- "white"
+    col_panel <- "gray20"
+    col_text <- "gray10"
+    col_lines <- "gray30"
+
+    output$distPlot <- renderPlot({
+        plot_map <- ggplot(data = selected_animal_year()) +
+        # Background map
+        #geom_polygon(data = london, aes(long, lat, group = group), fill = "grey30", color = "grey40") +
+        # Points for animal rescues
+        geom_point(aes(x = easting_rounded, y = northing_rounded, fill = special_service_type_category),
+                   pch = 21, color = "black", size = 5) +
+        scale_fill_manual(values = col_points) +
+        labs(title = "LONDON ANIMAL RESCUES",
+             subtitle = "<span style='color:#899DA4;'>below ground   </span>
+                 <span style='color:#C93312;'>from a hight   </span>
+                 <span style='color:#FAEFD1;'>from water   </span>
+                 <span style='color:#DC863B;'>other</span>") +
+        coord_equal() +
+        my_basic_theme(base_family = f2) +
+        theme(text = element_text(family = f2, size = 18, color = col_text),
+              plot.title = element_markdown(family = f1,
+                                            size = 40,
+                                            face = "bold",
+                                            margin = margin(b = 20)),
+              plot.subtitle = element_markdown(size = 23,
+                                               face = "bold",
+                                               margin = margin(b = 10)),
+              plot.caption = element_text(size = 12,
+                                          margin = margin(t = 10)),
+              axis.text = element_blank(),
+              axis.ticks = element_blank(),
+              plot.background = element_rect(fill = col_bg, color = col_bg),
+              panel.background = element_rect(fill = col_panel, color = col_panel),
+              legend.position = "none",
+              panel.spacing = unit(.05, "lines"),
+              panel.border = element_rect(color = col_lines, fill = NA, size = 1))
+
+        plot_time <- ggplot(across_time(), aes(x = cal_year, fill = special_service_type_category)) +
+            geom_bar(width = 0.5) +
+            gghighlight(cal_year == input$year) +
+            scale_fill_manual(values = col_points) +
+            scale_x_continuous(breaks = 2009:2020) +
+            labs(caption = "Source: London.gov  |  Visualization: Emma Skarstein") +
+            my_basic_theme(base_family = f2) +
+            theme(plot.caption = element_text(size = 12, margin = margin(t = 20)),
+                  axis.title = element_blank(),
+                  axis.text.x = element_text(family = f2),
+                  plot.background = element_rect(fill = col_bg, color = col_bg),
+                  panel.background = element_rect(fill = col_panel, color = col_panel),
+                  legend.position = "none",
+                  panel.spacing = unit(.05, "lines"),
+                  panel.border = element_rect(color = col_lines, fill = NA, size = 1),
+                  aspect.ratio = 0.3)
+        plot_map / plot_time
+    }, width = 600, height = 800)
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
