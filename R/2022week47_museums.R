e5437c40f68d1632b35d9bf22e9dea10781d0fcc
Emma Sofie Skarstein <emma@skarstein.no>
2022-12-08 15:56:56 +0100

    Create 2022week47_museums.R

---
+++ R/2022week47_museums.R
@@ -0,0 +1,6 @@
+# Museums, TidyTuesday 2022, week 47
+# By Emma Skarstein, November 2022
+
+
+
+museums <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-11-22/museums.csv')
